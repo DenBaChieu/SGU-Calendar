911ea6e0accde51ecb7d87dbe91ecba6e3a8ead1
Tran Anh Huy <Anhhuyc5@gmail.com>
2025-05-08 23:07:12 +0700

    Fixed backend link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function addEvent() {
     let eventTitle = document.getElementById("eventTitle").value;
 
-    fetch("https://your-backend.onrender.com/add-event", {
+    fetch("https://sgu-calendar.onrender.com/add-event", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ title: eventTitle })
